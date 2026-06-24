5f6eb62255d5a32ebf1f57068b8769af121ec8fd
Marius <31125272+vplkos@users.noreply.github.com>
2026-06-24 17:06:25 +0800

    Update busuu.js

--- scripts/busuu.js
+++ scripts/busuu.js
@@ -9,17 +9,16 @@ hostname = api.busuu.com
 if ($response && $response.body) {
     let body = $response.body;
 
-    // 100% 还原原版老脚本的替换规则与目标值（无任何魔改，保持原版的大力出奇迹逻辑）
     body = body
-        .replace(/"tier":"\w+"/g, '"tier":"premium"')
-        .replace(/"free_trial_eligible":\w+/g, '"free_trial_eligible":false')
-        .replace(/"is_premium":\w+/g, '"is_premium":true')
-        .replace(/"free_trial\":\w+/g, '"free_trial":false')
-        .replace(/"expiration":\d+/g, '"expiration":4092599349')
-        .replace(/"next_charge":\d+/g, '"next_charge":4092599349')
-        .replace(/"name":".*?"/g, '"name":"Premium"'); // 原版其实改成了 Premium，你想改彭于晏可以自己换掉
+      .replace(/"tier":"\w+"/g, '"tier":"premium"')
+      .replace(/"free_trial_eligible":\w+/g, '"free_trial_eligible":false')
+      .replace(/"is_premium":\w+/g, '"is_premium":true')
+      .replace(/"free_trial":\w+/g, '"free_trial":false')
+      .replace(/"expiration":\d+/g, '"expiration":9999999999')
+      .replace(/"next_charge":\d+/g, '"next_charge":0')
+      .replace(/"name":".*?"/g, '"name":"premium"'); // 想换回彭于晏可以在这里改
 
-    $done({ body: body });
+    $done({ body });
 } else {
     $done({});
 }
